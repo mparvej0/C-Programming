c018bbc6dabacbfc4d1937e6e626215e168e274f
MD PARVEJ <mparvejind@gmail.com>
2026-01-09 07:43:51 +0530

    .

---
+++ Introduction to Programming/Practice_set.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    printf("My name is Raju\n");
+    printf("I am 20 years old\n");
+    printf("Success is the sum of small efforts repeated day in and day out.\n");
+    printf("Hello, C Learner!\nWelcome to the world of programming.\n");
+    printf("I am learning C programming.\n");
+    printf("It feels exciting.\n");
+    printf("This is just the beginning!\n");
+    return 0;
+}
+g
